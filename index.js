7ddacdd1ea38ec4e17cdc2ee2ad6f2ce9526b52b
Rene Ramirez <rdejramirez@gmail.com>
2021-10-12 01:37:41 -0500

    enviroment

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import morgan from 'morgan';
+import dotenv from 'dotenv';
 
 const app = express();
+dotenv.config();
 const PORT = process.env.PORT || 5000;
 
 app.use(morgan('dev'));
